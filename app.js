70a6c07f3de51086294974082f67c27077647735
evaaaaawu <yiju.wu1@gmail.com>
2023-05-15 09:57:41 +0800

    feat: connect mongodb and add mongoose

--- app.js
+++ app.js
@@ -2,6 +2,12 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 
+// 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+require('./config/mongoose')
 const routes = require('./routes')
 
 const app = express()
