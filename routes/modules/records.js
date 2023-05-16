ad95bb2072eed0e67bc0e8980a2dd949cfe5b6bd
evaaaaawu <yiju.wu1@gmail.com>
2023-05-16 11:50:17 +0800

    feat: add edit page and routes

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -48,6 +48,69 @@ router.post('/new', (req, res) => {
 })
 
 // Update
+router.get('/edit/:id', (req, res) => {
+  const userId = req.user._id
+  const _id = req.params.id
+
+  Category.find()
+    .lean()
+    .then(catagories => {
+      return Record.findOne({ _id, userId })
+        .lean()
+        .then(item => {
+          catagories.forEach(category => {
+            if (String(category._id) === String(item.categoryId)) {
+              category.selected = true
+            } else {
+              category.selected = false
+            }
+          })
+          item.date = moment(item.date).format('YYYY-MM-DD')
+          res.render('edit', { item, catagories })
+        })
+        .catch(err => console.log(err))
+    })
+})
+
+router.put('/:id', (req, res) => {
+  Category.find()
+    .lean()
+    .then(catagories => {
+      const _id = req.params.id
+      const userId = req.user._id
+      const { name, date, categoryId, amount } = req.body
+      const errors = []
+
+      catagories.forEach(category => {
+        if (String(category._id) === categoryId) {
+          category.selected = true
+        } else {
+          category.selected = false
+        }
+      })
+
+      if (!name || !date || !categoryId || !amount) {
+        errors.push({ message: '所有欄位都是必填！' })
+      }
+      if (errors.length) {
+        return Record.findOne({ _id, userId })
+          .lean()
+          .then(item => {
+            item.date = moment(item.date).format('YYYY-MM-DD')
+            res.render('edit', {
+              errors, item, catagories
+            })
+          })
+      }
+
+      return Record.findOneAndUpdate({ _id, userId }, {
+        name, date, categoryId, amount, userId
+      })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
+})
 
 // Delete
 
